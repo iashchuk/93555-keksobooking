178bf96911fc08b0b23ee4b42b1daf2598375843
iashchuk <221box@list.ru>
2018-06-18 03:21:28 +0300

    Форма. Бета-версия

---
+++ js/form.js
@@ -0,0 +1,110 @@
+'use strict';
+
+var form = document.querySelector('.ad-form');
+var titleInput = form.querySelector('#title');
+var inputType = form.querySelector('#type');
+var inputPrice = form.querySelector('#price');
+var inputTimeIn = form.querySelector('#timein');
+var inputTimeOut = form.querySelector('#timeout');
+var inputRooms = form.querySelector('#room_number');
+var inputGuests = form.querySelector('#capacity');
+var optionGuests = inputGuests.querySelectorAll('option');
+var formReset = form.querySelector('.ad-form__reset');
+
+
+var TypePrice = {
+  'bungalo': 0,
+  'flat': 1000,
+  'house': 5000,
+  'palace': 10000
+};
+
+var RoomGuests = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0']
+};
+
+var invalidInputs = [];
+
+
+// Функция установки минимальных цен в зависимости от типа дома
+var onTypeInputChange = function () {
+  inputPrice.min = TypePrice[inputType.value];
+  inputPrice.placeholder = TypePrice[inputType.value];
+};
+
+// Функция синхронного изменение времени заезда и выезда
+var onTimeInputChange = function (input, value) {
+  input.value = value;
+};
+
+// Функция установки количества комнат, взависимости от числа гостей
+var onRoomInputChange = function () {
+  var room = inputRooms[inputRooms.selectedIndex].value;
+  var selectedValue = RoomGuests[room];
+
+  optionGuests.forEach(function (option) {
+    if (selectedValue.includes(option.value)) {
+      option.disabled = false;
+    } else {
+      option.selected = false;
+      option.disabled = true;
+    }
+  });
+};
+
+/**
+ * Функция выделение неправильно заполненных полей
+ * @param {Node} input
+ */
+var markInvalidInput = function (input) {
+  invalidInputs.push(input);
+  input.classList.add('invalid-field');
+};
+
+/**
+ * Функция снятия выделения с неправильно заполенных полей
+ * @param {Node} input
+ */
+var removeMarkInvalidInput = function (input) {
+  invalidInputs.splice(invalidInputs.indexOf(input), 1);
+  input.classList.remove('invalid-field');
+};
+
+// Функция проверки валидности полей
+var onInputCheckValidity = function (evt) {
+  var target = evt.target;
+  if (!target.checkValidity()) {
+    markInvalidInput(target);
+  } else if (invalidInputs.indexOf(target) !== -1) {
+    removeMarkInvalidInput(target);
+  }
+};
+
+// Функция очистки формы
+var clearForm = function () {
+  invalidInputs.forEach(function (input) {
+    input.classList.remove('invalid-field');
+  });
+  form.reset();
+};
+
+titleInput.addEventListener('change', onInputCheckValidity);
+inputType.addEventListener('change', onTypeInputChange);
+inputPrice.addEventListener('change', onInputCheckValidity);
+inputTimeIn.addEventListener('change', function () {
+  onTimeInputChange(inputTimeOut, inputTimeIn.value);
+});
+inputTimeOut.addEventListener('change', function () {
+  onTimeInputChange(inputTimeIn, inputTimeOut.value);
+});
+inputRooms.addEventListener('change', onRoomInputChange);
+inputGuests.addEventListener('click', onRoomInputChange);
+formReset.addEventListener('click', function () {
+  clearForm();
+});
+form.addEventListener('invalid', function (evt) {
+  markInvalidInput(evt.target);
+}, true);
